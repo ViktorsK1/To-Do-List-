4a800d946008c7ff6cf4090a2fac97abae1e0d73
Viktor Kulia <vitya.kulya@gmail.com>
2023-02-19 20:42:19 +0200

    Added a ViewModel with @EnvironmentObject
    - created CRUD with data from array
    - created alert on AddView

--- To-Do-List/To-Do-List/To_Do_ListApp.swift
+++ To-Do-List/To-Do-List/To_Do_ListApp.swift
@@ -18,11 +18,15 @@ import SwiftUI
 
 @main
 struct To_Do_ListApp: App {
+    
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 ListView()
             }
+            .environmentObject(listViewModel)
         }
     }
 }

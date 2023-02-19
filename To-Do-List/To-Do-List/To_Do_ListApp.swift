33807d66b803bf1e400b6d263858422defe0805a
Viktor Kulia <vitya.kulya@gmail.com>
2023-02-19 11:30:39 +0200

    init project

---
+++ To-Do-List/To-Do-List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do-List
+//
+//  Created by Виктор Куля on 19.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
